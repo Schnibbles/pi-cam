c5916b8a27ba013c053f5c85f6a3aae7fb0d78bc
Schnibbles <88000402+Schnibbles@users.noreply.github.com>
2023-04-02 17:34:12 +0100

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -9,3 +9,7 @@ pj = PiJuice(1,0x14)
 chargelevel = pj.status.GetChargeLevel()
 batterystatus = list(chargelevel.values())[0]
 print(batterystatus)
+
+url = "192.168.1.131/api/webhook/pi-cambatterypercentage"
+post = requests.post(url,batterystatus)
+

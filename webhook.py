bc24e6da3c91feb23b52586f789869cb8e4f94f1
Schnibbles <88000402+Schnibbles@users.noreply.github.com>
2023-04-03 09:14:40 +0100

    Fixing typos

--- webhook.py
+++ webhook.py
@@ -8,13 +8,13 @@ from pijuice import PiJuice
 pj = PiJuice(1,0x14)
 
 chargelevel = pj.status.GetChargeLevel()
-percentage = float(list(chargelevel.values())[0]
+percentage = float(list(chargelevel.values())[0])
 url = "http://192.168.1.131:8123/api/webhook/pi-cambatterypercentage"
 
 header = {
 "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2ODBiODZiNmQyYzc0MjY0OWVhZmMzMDVjNDExZTk2OCIsI>"}
 
-chargelevel = {"data": percentage)
+chargelevel = {"data": percentage}
 
 print(chargelevel)
 ha = requests.post(url,json = chargelevel,headers = header)
